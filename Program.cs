96cfc7f051ed034e8d51712eab187a80b1d1017b
Henrique Shoji <henrique.shoji@privacy.com.br>
2023-01-27 15:26:03 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,4 @@
+using AuthAPI;
+
+var builder = WebApplication.CreateBuilder(args)
+    .UseStartup<Startup>();
---
+++ Startup.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace AuthAPI
+{
+    public class Startup : IStartup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "BankApi", Version = "v1" });
+                c.ResolveConflictingActions(x => x.First());
+            });
+        }
+        public void Configure(WebApplication app, IWebHostEnvironment env)
+        {
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+        }
+    }
+
+    public interface IStartup
+    {
+        IConfiguration Configuration { get; }
+        void Configure(WebApplication app, IWebHostEnvironment environment);
+        void ConfigureServices(IServiceCollection services);
+    }
+
+    public static class StartupExtensions
+    {
+        public static WebApplicationBuilder UseStartup<TStartup>(this WebApplicationBuilder WebAppBuilder) where TStartup : IStartup
+        {
+            var startup = Activator.CreateInstance(typeof(TStartup), WebAppBuilder.Configuration) as IStartup;
+            if (startup == null) throw new ArgumentException("Classe Startup.cs inválida!");
+            startup.ConfigureServices(WebAppBuilder.Services);
+            var app = WebAppBuilder.Build();
+            startup.Configure(app, app.Environment);
+            app.Run();
+            return WebAppBuilder;
+        }
+    }
+}
